1a958490b0ee4731ba95027e9d8680004128c372
Ruxin Qu <rosheenqu@gmail.com>
2022-11-28 21:27:06 -0800

    monkey will close eyes when typing passwords

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,8 @@
 $(document).ready(()=>{
     $('#password').focus(function(){
         $('.monkey').attr('src','/login-img/close.jpg');
-    })
-    $('.monkey').attr('src','/login-img/open.JPG');
-})
+    });
+    $('#password').blur(function(){
+        $('.monkey').attr('src','/login-img/open.JPG');
+    });
+});
