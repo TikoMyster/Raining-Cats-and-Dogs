b27aa717364e5baa8035f461be0fa8e07cf3cb4b
Ruxin Qu <rosheenqu@gmail.com>
2022-11-28 19:52:48 -0800

    fix bugs on home page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { Hotel } = require('../models');
 const withAuth = require('../utils/auth');
 
-
 router.get('/', async (req, res) => {
     try {
         // Get all hotels and JOIN with user data
