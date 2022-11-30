c4fd0e0256f32b704955a8626c8c89762bbafe4e
Ruxin Qu <rosheenqu@gmail.com>
2022-11-29 17:34:43 -0800

    move the background image to be on top of the banner

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@ const changeBg = async () => {
         const response = await fetch('https://dog.ceo/api/breeds/image/random');
         if (response.ok) {
             const jsonResponse = await response.json();
-            document.body.style.setProperty('--bg-img', `url(${jsonResponse.message})`)
+            document.getElementById('top-banner-bg-image').style.setProperty('--bg-img', `url(${jsonResponse.message})`);
         }
     } catch (err) { console.log(err); }
 };
